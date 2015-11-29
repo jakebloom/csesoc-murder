e728e119f388490bb6242d4483e5dd1bb9b4e686
Jake Bloom <jakebloom744@gmail.com>
2015-11-29 16:45:15 +1100

    endpoints now need auth

--- models/User.js
+++ models/User.js
@@ -4,12 +4,13 @@ var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
 	name: String,
-	username: (type: String, lowercase: true, unique: true},
+	username: {type: String, lowercase: true, unique: true},
 	hash: String,
 	salt: String,
 	codeword: String,
 	target: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-	alive: { type: Boolean, default: true }
+	alive: { type: Boolean, default: true },
+	admin: { type: Boolean, default: false}
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var jwt = require('express-jwt');
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
 
-/* GET users listing. */
+var User = mongoose.model('User');
+var auth = jwt({secret: process.env.MURDER_SECRET, userProperty: 'payload'});
+/* GET users listing. Remove this soon.*/
 router.get('/', function(req, res, next) {
   
 	User.find(function(err, users){
@@ -14,15 +16,49 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-/* create a new user */
-router.post('/register', function(req, res, next) {
-	var user = new User(req.body);
+/* view yourself */
+router.get('/me', auth, function(req, res, next){
+	var username = req.payload.username;
+	User.findOne({'username': username}, function(err, user){
+		if (err){return err;}
 
-	user.save(function(err, user){
-		if (err) {return next(err);}
-
-		res.json(user);
+		return res.json(user);
 	});
+});
+
+/* create a new user */
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+});
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
