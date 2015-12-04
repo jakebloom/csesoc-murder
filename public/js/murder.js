70fb85b00eb8494fe406067f1f5bc2223012aa75
Jake Bloom <jakebloom744@gmail.com>
2015-12-04 21:37:02 +1100

    Angular UI router

--- public/js/murder.js
+++ public/js/murder.js
@@ -1,1 +1,22 @@
-var app = angular.module('murder', []);
+var app = angular.module('murder', ['ui.router']);
+
+
+app.controller('MainCtrl', [function(){}]);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+
+			.state('home', {
+				url: '/',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			});
+
+
+		$urlRouterProvider.otherwise('/');
+	}
+]);
