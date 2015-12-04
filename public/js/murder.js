59fcf3dca75c229a1e9d3e57f986ce63cd0acf0f
Jake Bloom <jakebloom744@gmail.com>
2015-12-05 07:56:26 +1100

    Added authentication controller, service and views

--- public/js/murder.js
+++ public/js/murder.js
@@ -4,12 +4,89 @@ app.factory('users', ['$http', function(){
 	var o = {
 		users: []
 	};
+
+	o.getAll = function() {
+		return $http.get('/users').success(function(data){
+			angular.copy(data, o.users);
+		});
+	};
+
 	return o;
+}]);
+
+app.factory('auth', ['$http', '$window', function($http, $window){
+	var auth = {};
+
+	auth.saveToken = function(token){
+		$window.localStorage['murder-token'] = token;
+	}
+
+	auth.getToken = function(){
+		return $window.localStorage['murder-token'];
+	}
+
+	auth.isLoggedIn = function(){
+		var token = auth.getToken();
+
+		if (!token){return false;}
+
+		var payload = JSON.parse($window.atob(token.split('.')[1]));
+	}
+
+	auth.currentUser = function(){
+		if(auth.isLoggedIn()){
+			var token = auth.getToken();
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+			return payload.username;
+		}
+	};
+
+	auth.login = function(user){
+		return $http.post('/users/login', user).success(function(data){
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logout = function(){
+		$window.localStorage.removeItem['murder-token'];
+	}
+
+	auth.register = function(){
+		return $http.post('/users/register', user).success(function(data){
+			auth.saveToken(data.token);
+		});
+	}
+
+	return auth;
 }]);
 
 app.controller('MainCtrl', [function(){}]);
 
-app.controller('RegisterCtrl', [function(){}]);
+app.controller('AuthCtrl', [
+	'$scope', '$state', 'auth',
+	function($scope, $state, auth){
+		$scope.user = {};
+
+		$scope.register = function(){
+			auth.register($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				$state.go('home');
+			});
+		};
+
+		$scope.login = function(){
+			auth.login($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				$state.go('home');
+			});
+		};
+
+
+	}]
+);
 
 app.config([
 	'$stateProvider',
@@ -27,8 +104,24 @@ app.config([
 			.state('register', {
 				url: '/register',
 				templateUrl: '/register.html',
-				controller: 'RegisterCtrl'
+				controller: 'AuthCtrl',
+				onEnter: ['$state', 'auth', function($state, $auth){
+					if(auth.isLoggedIn()){
+						$state.go('home');
+					}
+				}]
 			});
+
+			.state('login', {
+				url: '/login',
+				templateUrl: '/login.html',
+				controller: 'AuthCtrl',
+				onEnter: ['$state', 'auth', function($state, auth){
+					if(auth.isLoggedIn()){
+						$state.go('home');
+					}
+				}]
+			})
 
 
 		$urlRouterProvider.otherwise('/');
