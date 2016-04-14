ddfcde437f118602c42ef23e798b48be0ea043f8
jake-waratahs <jakebloom744@gmail.com>
2016-04-14 15:31:16 +0300

    Can assign codewords

--- views/modules/Admin.js
+++ views/modules/Admin.js
@@ -1,16 +1,51 @@
 import React from 'react'
 import MessageBox from './MessageBox.js'
+import $ from 'jquery'
 
 export default React.createClass({
+
+	getInitialState(){
+		return {
+			message: {}
+		}
+	},
+
+	setCodewords() {
+		$.ajax({
+			type: "GET",
+			url: "/admin/assign",
+			headers: {
+				Authorization: 'Bearer ' + localStorage.jwt || ""
+			},
+			success: function(data) {
+				this.setState({
+					message: {
+						type: "success",
+						message: data.message
+					}
+				})
+			}.bind(this),
+			error: function(data) {
+				this.setState({
+					message: {
+						type: "danger",
+						message: data.message
+					}
+				})				
+			}.bind(this)
+		})
+	},
+
+	startGame() {},
+
 	render() {
 		return (
 			<div>
 				<h3>Admin</h3>
-				<MessageBox type="danger" />
-				<MessageBox type="success" />
+				<MessageBox type={this.state.message.type} message={this.state.message.message} />
 				
 				<div className="form-group">
-					<button className="btn btn-default btn-primary">Assign codewords</button>
+					<button className="btn btn-default btn-primary" onClick={this.setCodewords}>Assign codewords</button>
 				</div>
 				<div className="form-group">
 					<button className="btn btn-default btn-danger">Start new game</button>
