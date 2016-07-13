25590b6184019b4f5fd35cfe3dc278bb4aefbc11
jake-waratahs <jakebloom744@gmail.com>
2016-07-13 14:50:17 +1000

    Added numbered url

--- models/Kill.js
+++ models/Kill.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose')
 
 var KillSchema = new mongoose.Schema({
 	killer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-	victim: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null }
+	victim: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
+	time: { type: Date, default: Date.now }
 });
 
 mongoose.model('Kill', KillSchema)
--- routes/kills.js
+++ routes/kills.js
@@ -8,16 +8,24 @@ router.get('/', function(req, res, next){
 	Kill.find().lean()
 		.populate('killer', 'name').populate('victim', 'name')
 		.exec(function(err, kills){
-			console.log("Got here");
 			if (err) {return next(err);}
+			return res.json(kills);
+		}
+	);
+});
 
+router.param('num', function(req, res, next, num){
+	Kill.find().sort('-time').lean()
+		.populate('killer', 'name').populate('victim', 'name')
+		.limit(parseInt(num)).exec(function(err, kills){
+			if (err) {return next(err);}
 			return res.json(kills);
 		}
 	);
 });
 
 router.get('/:num', function(req, res, next){
-	return res.json([]);
+	return res.ok();
 });
 
 module.exports = router;
